04525192697b330d696160e286ab9fe34061bc48
MatchaWu <matchawu@users.noreply.github.com>
2018-06-04 19:43:43 +0800

    Add files via upload

---
+++ test_webhook.js
@@ -0,0 +1,65 @@
+'use strict'
+const line = require('./index')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+// need raw buffer for signature validation
+app.use(bodyParser.json({
+  verify (req, res, buf) {
+    req.rawBody = buf
+  }
+}))
+
+// init with auth
+line.init({
+  accessToken: 'qF++eglf2AIPhP/dk6znNJmKvLCVIquvaOobAvuGVGlC1AGEALKEP8fbpXReslDX/EqEmFT3LjOyK7izVlt0If848ShqSLFP+0D8R/fxA5kTkFnZKFB+0I+4Fiv3wYoRu0O/FtRi1cKHAjmXtQP57QdB04t89/1O/w1cDnyilFU=',
+  // (Optional) for webhook signature validation
+  channelSecret: '0538eeffbcbca655526b03782cedcdf3'
+})
+
+/**
+ * response example (https://devdocs.line.me/ja/#webhook):
+ * {
+ *   "events": [
+ *     {
+ *       "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+ *       "type": "message",
+ *       "timestamp": 1462629479859,
+ *       "source": {
+ *         "type": "user",
+ *         "userId": "u206d25c2ea6bd87c17655609a1c37cb8"
+ *       },
+ *       "message": {
+ *         "id": "325708",
+ *         "type": "text",
+ *         "text": "Hello, world"
+ *       }
+ *     }
+ *   ]
+ * }
+ */
+
+app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
+  // get content from request body
+  const promises = req.body.events.map(event => {
+    // reply message
+    return line.client
+      .replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+  })
+  Promise
+    .all(promises)
+    .then(() => res.json({success: true}))
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Example app listening on port 3000!')
+})
